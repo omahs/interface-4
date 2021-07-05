f0521ef582590bbcffe1a8cf5edb0d5631d6286b
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-06 01:35:30 +0200

    added handleSubmit utils

---
+++ utils/handleSubmit.ts
@@ -0,0 +1,24 @@
+import { Dispatch, SetStateAction } from "react"
+import handleMessage, { Message } from "./handleMessage"
+
+const handleSubmit = async (
+  action: Promise<any>,
+  e: React.SyntheticEvent<EventTarget>,
+  setMessage: Dispatch<SetStateAction<Message>>,
+  setLoading: Dispatch<SetStateAction<boolean>>
+) => {
+  e.preventDefault()
+  setLoading(true)
+  setMessage({ message: "", messageStatus: "success" })
+  try {
+    await action
+    handleMessage({ message: "Success!", messageStatus: "success" }, setMessage)
+  } catch (err) {
+    const message = err.data.message.split("'")[1]
+    handleMessage({ message, messageStatus: "error" }, setMessage)
+  }
+
+  setLoading(false)
+}
+
+export default handleSubmit
