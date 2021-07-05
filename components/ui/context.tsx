ab3ec16fbee6a62bfcb48457d46f4f6e5d5fd252
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-05 15:31:41 +0200

    random colors in context

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,14 +1,25 @@
 import useProvider from "@lib/useProvider"
 import { createContext, useContext, useState } from "react"
+import colorList from "@utils/colorList"
 
-const AppContext = createContext({ isConnected: false, loading: true })
+const AppContext = createContext({
+  isConnected: false,
+  loading: true,
+  color1: colorList[0],
+  color2: colorList[1],
+})
 
 export function AppWrapper({ children }) {
   const [loading, setLoading] = useState(true)
   const isConnected = useProvider(setLoading)
 
+  const color1 = colorList[Math.floor(Math.random() * colorList.length)]
+  const color2 = colorList[Math.floor(Math.random() * colorList.length)]
+  // const colorDark1 = colorListDark[Math.floor(Math.random() * colorListDark.length)]
+  // const colorDark2 = colorListDark[Math.floor(Math.random() * colorListDark.length)]
+
   return (
-    <AppContext.Provider value={{ isConnected, loading }}>
+    <AppContext.Provider value={{ isConnected, loading, color1, color2 }}>
       {children}
     </AppContext.Provider>
   )
---
+++ utils/colorList.ts
@@ -0,0 +1,67 @@
+import colors from "tailwindcss/colors"
+
+const colorList = [
+  [
+    colors.cyan[300],
+    "bg-cyan-300",
+    "text-cyan-300",
+    "from-cyan-300",
+    "to-cyan-300",
+  ],
+  [
+    colors.green[300],
+    "bg-green-300",
+    "text-green-300",
+    "from-green-300",
+    "to-green-300",
+  ],
+  [
+    colors.purple[300],
+    "bg-purple-300",
+    "text-purple-300",
+    "from-purple-300",
+    "to-purple-300",
+  ],
+  [
+    colors.indigo[300],
+    "bg-indigo-300",
+    "text-indigo-300",
+    "from-indigo-300",
+    "to-indigo-300",
+  ],
+  [
+    colors.yellow[300],
+    "bg-yellow-300",
+    "text-yellow-300",
+    "from-yellow-300",
+    "to-yellow-300",
+  ],
+  [colors.red[300], "bg-red-300", "text-red-300", "from-red-300", "to-red-300"],
+  [
+    colors.pink[300],
+    "bg-pink-300",
+    "text-pink-300",
+    "from-pink-300",
+    "to-pink-300",
+  ],
+  [
+    colors.blue[300],
+    "bg-blue-300",
+    "text-blue-300",
+    "from-blue-300",
+    "to-blue-300",
+  ],
+  [colors.sky[300], "bg-sky-300", "text-sky-300", "from-sky-300", "to-sky-300"],
+]
+
+// const colorListDark = [
+// colors.cyan[600],
+// colors.purple[600],
+// colors.indigo[600],
+// colors.yellow[600],
+// colors.red[600],
+// colors.pink[600],
+// colors.blue[600],
+// ]
+
+export default colorList
