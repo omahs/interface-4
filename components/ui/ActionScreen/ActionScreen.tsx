25e557f1079029546ae183bf53b37bc31b7fd25e
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-17 18:18:43 +0200

    changed unauthorizedBlock for ActionScreen component

---
+++ components/ui/ActionScreen/ActionScreen.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react"
+import { Button } from "@components/ui"
+import { NextSeo } from "next-seo"
+
+export interface ActionScreenProps {
+  text: string
+  buttonLabel?: string
+  title?: string
+  helpText?: string
+  href?: string
+  onClick?: any
+}
+
+const ActionScreen: FC<ActionScreenProps> = ({
+  text,
+  buttonLabel,
+  title,
+  helpText,
+  href,
+  onClick,
+}) => {
+  return (
+    <>
+      {title && <NextSeo title={title} />}
+      <div className="flex flex-col items-center text-center">
+        <h3 className="font-semibold mb-7">{text}</h3>
+        {helpText && <p className="opacity-70 mb-7">{helpText}</p>}
+        {buttonLabel &&
+          (href ? (
+            <Button label={buttonLabel} href={href} />
+          ) : (
+            <Button label={buttonLabel} onClick={onClick} />
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default ActionScreen
