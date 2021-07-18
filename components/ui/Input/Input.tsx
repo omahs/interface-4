0a1a55abae67e74a2eeb5c1d1377fbce7893392c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-18 02:52:32 +0200

    added label to Input

--- components/ui/Input/Input.tsx
+++ components/ui/Input/Input.tsx
@@ -3,14 +3,14 @@ import React, { InputHTMLAttributes } from "react"
 
 export interface Props extends InputHTMLAttributes<HTMLInputElement> {
   className?: string
+  label?: string
   onChange?: (...args: any[]) => any
 }
 
 const Input: React.FC<Props> = (props) => {
-  const { className, children, onChange, ...rest } = props
+  const { className, onChange, label, children, ...rest } = props
 
   const rootClassName = `focus:border-sky-600 shadow-light-focusable bg-white rounded-t-sm py-2 pl-5 w-full appearance-none transition duration-150 ease-in-out pr-3 border-b-[3px] placeholder-gray-400 border-blue-300 text-black focus:outline-none ${className}`
-  // const rootClassName = `focus:ring-2 shadow-medium ring-sky-300 bg-white rounded-sm py-2 pl-5 w-full appearance-none transition duration-150 ease-in-out pr-3 border placeholder-gray-400 border-gray-200 text-black focus:outline-none ${className}`
 
   const handleOnChange = (e: any) => {
     if (onChange) {
@@ -21,6 +21,11 @@ const Input: React.FC<Props> = (props) => {
 
   return (
     <label>
+      {label && (
+        <p className="pb-1.5 text-sm font-semibold text-left text-gray-700">
+          {label}
+        </p>
+      )}
       <input
         className={rootClassName}
         onChange={handleOnChange}
