e493513b68bba4f645e9602dd0f07ac6edd63952
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-23 01:50:30 +0200

    fix

--- pages/api/slicer/index.tsx
+++ pages/api/slicer/index.tsx
@@ -7,8 +7,10 @@ type Data = {
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const totalSlicers = await slice(defaultProvider).totalTokens()
-  res.status(200).json({ totalSlicers: Number(totalSlicers) })
+  if (req.method === "GET") {
+    const totalSlicers = await slice(defaultProvider).totalTokens()
+    res.status(200).json({ totalSlicers: Number(totalSlicers) })
+  }
 }
 
 export default handler
