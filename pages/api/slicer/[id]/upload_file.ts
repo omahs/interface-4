e69fb5d11746b5403e58402b6b3faf9cfe6411e7
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-26 01:12:51 +0200

    fixed upload_file

--- pages/api/slicer/[id]/upload_file.ts
+++ pages/api/slicer/[id]/upload_file.ts
@@ -1,3 +1,4 @@
+import fs from "fs"
 import fetcher from "@utils/fetcher"
 const sharp = require("sharp")
 import type { NextApiRequest, NextApiResponse } from "next"
@@ -5,20 +6,21 @@ import type { NextApiRequest, NextApiResponse } from "next"
 export const config = {
   api: {
     bodyParser: {
-      sizeLimit: "10mb",
+      sizeLimit: "20mb",
     },
   },
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query
-  console.log(JSON.parse(req.body))
   const { buffer, fileExt } = JSON.parse(req.body)
+
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
   const supabaseKey = process.env.SUPABASE_KEY
   const supabaseStorage = "dev"
+  const fileName = `slicer_${id}_main`
 
-  const imageBody = (buf: Buffer) => ({
+  const imageBody = (buf: Buffer | File) => ({
     method: req.method,
     headers: {
       authorization: `Bearer ${supabaseKey}`,
@@ -29,25 +31,34 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
     if (req.method === "POST" || req.method === "PUT") {
-      const buf = Buffer.from(buffer, "binary")
+      const buf = Buffer.from(buffer, "latin1")
 
-      const image = await sharp(buf)
-        .resize({ width: 800, withoutEnlargement: true })
-        .toBuffer(`slicer_${id}_main.${fileExt}`)
+      if (fileExt === "gif") {
+        await sharp(buf, { animated: true })
+          // .resize({ width: 800, withoutEnlargement: true })
+          // .gif({ pageHeight: 460 })
+          .toFile(`${fileName}.webp`)
+      } else {
+        await sharp(buf)
+          .resize({ width: 800, withoutEnlargement: true })
+          .toFile(`${fileName}.webp`)
+      }
+      await sharp(buf).resize(64).toFile(`${fileName}_blur.webp`)
+
+      const file = fs.readFileSync(`${fileName}.webp`)
+      const fileBlur = fs.readFileSync(`${fileName}_blur.webp`)
 
       const { Key } = await fetcher(
-        `${supabaseUrl}/storage/v1/object/${supabaseStorage}/slicer_${id}_main`,
-        imageBody(image)
+        `${supabaseUrl}/storage/v1/object/${supabaseStorage}/${fileName}`,
+        imageBody(file)
       )
-
-      const imageRed = await sharp(buf)
-        .resize(6)
-        .toBuffer(`slicer_${id}_main_blur.${fileExt}`)
-
       await fetcher(
-        `${supabaseUrl}/storage/v1/object/${supabaseStorage}/slicer_${id}_main_blur`,
-        imageBody(imageRed)
+        `${supabaseUrl}/storage/v1/object/${supabaseStorage}/${fileName}_blur`,
+        imageBody(fileBlur)
       )
+
+      fs.unlinkSync(`${fileName}.webp`)
+      fs.unlinkSync(`${fileName}_blur.webp`)
 
       // Log if error
       // const { error: uploadError } = await supabase.storage ...
