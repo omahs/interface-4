ad08f2cff27700f8b97b93a39f20bfe607e8b983
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-26 20:55:36 +0200

    added homepage texts

---
+++ lib/text/home.tsx
@@ -0,0 +1,82 @@
+import { DoubleText } from "@components/ui"
+
+export const section1 = (
+  <>
+    <h1 className="!mb-2">
+      Meet the <DoubleText inactive logoText="Slicer" size="text-normal" />
+    </h1>
+    <p>
+      An autonomous being on the Ethereum blockchain (smart contract) capable of
+      transparently distributing its ETH to its owners, proportionally to their
+      owned <DoubleText logoText="slices 🍰" size="text-normal" inactive />
+    </p>
+    <p>
+      Since it&apos;s a special kind of{" "}
+      <DoubleText inactive logoText="fractionable-NFT" size="text-normal" />,
+      you&apos;re free to transfer/sell your slices however you prefer (even on
+      Opensea) and the slicer will always give you what you own.
+    </p>
+  </>
+)
+
+export const section2 = (
+  <>
+    <h1 className="!mb-2">
+      {" "}
+      <DoubleText inactive logoText="NFTs" size="text-normal" /> at their full
+      potential
+    </h1>
+    <p>
+      NFTs are cool because they are unique, so they can digitally represent any
+      real or virtual asset, service or product. However, they&apos;re used
+      mainly for collecting purposes as it&apos;s hard to make them useful in
+      real world applications.
+    </p>
+    <p>
+      Slicers ownership is linked to their value so they are able to{" "}
+      <DoubleText
+        inactive
+        logoText="truly represent anything"
+        size="text-normal"
+      />
+      . Freely exchangeable slices, privileged access to restricted operations,
+      upgradeability and many more features to come.
+    </p>
+  </>
+)
+
+export const section3 = (
+  <>
+    <h1 className="!mb-2">What are slicers for?</h1>
+    <p>
+      For now slicers can be used to collect payments for a project or product,
+      and automatically split them between its owners.
+    </p>
+    <p>
+      But there is{" "}
+      <DoubleText inactive logoText="so much more" size="text-normal" /> coming
+      soon. Slicers will turn into an independent payment infrastructure for
+      your project, company, product, service or whatever.
+    </p>
+  </>
+)
+
+export const section4 = (
+  <>
+    <h1 className="!mb-2">
+      Decentralized & <DoubleText inactive logoText="Free" size="text-normal" />
+    </h1>
+    <p>
+      Slicers are completely free to use, except for the blockchain transaction
+      fees. There is just a tiny 2.5% fee which is automatically withheld to any
+      payment made to the slicer, which will be used to improve our services
+      over time.
+    </p>
+    <p>
+      As a reward for using slicers, you will receive{" "}
+      <DoubleText inactive logoText="SLC tokens" size="text-normal" /> (ERC20)
+      whenever you release ETH from a slicer, which can be used to unlock
+      services from Slice in the future.
+    </p>
+  </>
+)
