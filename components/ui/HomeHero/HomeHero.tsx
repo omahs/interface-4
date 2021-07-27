969421ac8f7673523cfb72e6277fd2675a2e382b
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-07-28 01:14:17 +0200

    added react-rotating-text to HomeHero

--- components/ui/HomeHero/HomeHero.tsx
+++ components/ui/HomeHero/HomeHero.tsx
@@ -2,25 +2,9 @@ import Link from "next/link"
 import Image from "next/image"
 import isthis from "public/isthis.jpeg"
 import { Button, DoubleText } from "@components/ui"
-import { useEffect, useState } from "react"
-
-const textCarousel = ["applications", "services", "everything"]
+import ReactRotatingText from "react-rotating-text"
 
 const HomeHero = () => {
-  const [text, setText] = useState("")
-
-  useEffect(() => {
-    setTimeout(() => {
-      setText(textCarousel[0])
-    }, 2000)
-    setTimeout(() => {
-      setText(textCarousel[1])
-    }, 4000)
-    setTimeout(() => {
-      setText(textCarousel[2])
-    }, 6000)
-  }, [])
-
   return (
     <div className="relative min-h-[440px] sm:min-h-[520px] flex flex-col justify-center pb-16 sm:pb-24">
       <div className="pb-6 sm:pb-6">
@@ -39,7 +23,13 @@ const HomeHero = () => {
       </div>
       <h2 className="pb-1 font-black leading-normal">
         Decentralised payments <br />
-        <span className="text-blue-600">for {text}</span>
+        <span className="text-blue-600">
+          for{" "}
+          <ReactRotatingText
+            pause={2000}
+            items={["applications", "services", "everything"]}
+          />
+        </span>
       </h2>
 
       <div className="flex flex-row items-center justify-start pt-8 space-x-12 space-y-0 sm:pt-10 sm:py-10">
