6f9f083061a48344cba75aaddf44bfc8735ddee4
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-08-01 21:42:54 +0200

    fix

--- pages/slicer/[id]/transfer.tsx
+++ pages/slicer/[id]/transfer.tsx
@@ -93,7 +93,8 @@ const Transfer = ({
 
 export async function getStaticPaths() {
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL
-  const { totalSlicers } = await fetcher(`${baseUrl}/api/slicer/total`)
+  // const { totalSlicers } = await fetcher(`${baseUrl}/api/slicer/total`)
+  const totalSlicers = 0
   const paths = [...Array(totalSlicers).keys()].map((slicerId) => {
     const id = String(slicerId)
     return {
