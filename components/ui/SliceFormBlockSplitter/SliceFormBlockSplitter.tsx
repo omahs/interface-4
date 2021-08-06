83accf73be83e221fe01640c5e81f38466c76023
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-08-07 00:24:24 +0200

    styling

--- components/ui/SliceFormBlockSplitter/SliceFormBlockSplitter.tsx
+++ components/ui/SliceFormBlockSplitter/SliceFormBlockSplitter.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link"
 import { useEffect, useState, Dispatch, SetStateAction } from "react"
 import { Input, SliceFormInputBlock, Question } from "@components/ui"
 import Add from "@components/icons/Add"
@@ -146,7 +145,7 @@ const SliceFormBlockSplitter = ({
           onChange={setMinimumShares}
         />
       </div>
-      <div className="col-start-6 pl-5 xs:pl-0 xs:col-span-1 flex items-center mt-1.5">
+      <div className="flex items-center col-start-6 pl-5 mb-3 xs:pl-0 xs:col-span-1">
         <p
           className={`text-sm font-bold ${
             minimumShares > totalShares ? "text-red-500" : ""
