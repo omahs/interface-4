e5369395df6156f1cb4cda5ee4032edf0538e3d6
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-08-16 20:14:33 +0200

    edit subgraphQuery utils

--- utils/subgraphQuery.ts
+++ utils/subgraphQuery.ts
@@ -1,35 +1,30 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  ApolloQueryResult,
-} from "@apollo/client"
+import { gql, ApolloQueryResult } from "@apollo/client"
+import client from "./apollo-client"
 import { Dispatch, SetStateAction, useEffect, useState } from "react"
 
 const graphQuery = async (
   tokensQuery: string,
   setData: Dispatch<SetStateAction<ApolloQueryResult<any>>>
 ) => {
-  const APIURL = process.env.NEXT_PUBLIC_THEGRAPH_API_URL
-
-  const client = new ApolloClient({
-    uri: APIURL,
-    cache: new InMemoryCache(),
-  })
-
   try {
     const query = await client.query({ query: gql(tokensQuery) })
-    setData(query)
+    setData(query.data)
   } catch (err) {
     console.log("Error fetching data: ", err)
   }
 }
 
-const useQuery = (tokensQuery: string) => {
+const useQuery = (tokensQuery: string, dependencies = []) => {
   const [data, setData] = useState(null)
   useEffect(() => {
-    graphQuery(tokensQuery, setData)
-  }, [])
+    let execute = true
+    dependencies.map((dep) => {
+      !dep ? (execute = false) : null
+    })
+    if (execute) {
+      graphQuery(tokensQuery, setData)
+    }
+  }, dependencies)
   return data
 }
 
