32c9fe453c8f31a333ecd6905754fff25486c159
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-08-29 01:30:38 +0200

    products/new wip

--- pages/slicer/[id]/products/new.tsx
+++ pages/slicer/[id]/products/new.tsx
@@ -1,4 +1,10 @@
-import { ConnectBlock, Container, DoubleText } from "@components/ui"
+import {
+  ActionScreen,
+  AddProductForm,
+  ConnectBlock,
+  Container,
+  DoubleText,
+} from "@components/ui"
 import { NextSeo } from "next-seo"
 import {
   defaultDescription,
@@ -8,11 +14,18 @@ import {
 } from "@components/common/Head"
 import { useAllowed } from "@lib/useProvider"
 import { useRouter } from "next/dist/client/router"
+import { useState } from "react"
+import { LogDescription } from "ethers/lib/utils"
+import getLog from "@utils/getLog"
 
 export default function NewProduct() {
   const router = useRouter()
   const { id } = router.query
-  const { state, loading } = useAllowed(Number(id))
+  const { isAllowed, loading } = useAllowed(Number(id))
+  const [loadingForm, setLoadingForm] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const [logs, setLogs] = useState<LogDescription[]>()
+  const eventLog = getLog(logs, "ProductAdded")
 
   return (
     <Container page={true}>
@@ -34,22 +47,56 @@ export default function NewProduct() {
       />
       <ConnectBlock>
         {loading ? (
-          <p>Loading</p>
-        ) : state ? (
-          <main className="max-w-[420px] mx-auto sm:max-w-screen-md">
-            <DoubleText
-              inactive
-              logoText="Add a new product"
-              size="text-4xl sm:text-5xl"
-              position="pb-12"
+          <p className="text-lg">Loading...</p>
+        ) : isAllowed ? (
+          !success ? (
+            !loadingForm ? (
+              <main className="max-w-[420px] mx-auto sm:max-w-screen-md">
+                <DoubleText
+                  inactive
+                  logoText="Add a new product"
+                  size="text-4xl sm:text-5xl"
+                  position="pb-12"
+                />
+                <AddProductForm
+                  success={success}
+                  setLoading={setLoadingForm}
+                  setSuccess={setSuccess}
+                  setLogs={setLogs}
+                />
+              </main>
+            ) : (
+              <ActionScreen
+                text="Adding product ..."
+                helpText="Please wait while the blockchain does its thing"
+                loading
+              />
+            )
+          ) : (
+            <ActionScreen
+              highlightTitle="Product added! 🍰"
+              helpText={
+                <p className="pb-6">
+                  You can find the new product with id{" "}
+                  <b>{eventLog && eventLog[0]}</b> in the slicer page.
+                </p>
+              }
+              buttonLabel="Go to slicer"
+              href={`/slicer/${id}`}
+              buttonLabelSecondary="Add a new product"
+              onClickSecondary={() => setSuccess(false)}
             />
-          </main>
+          )
         ) : (
-          <p>Not allowed</p>
+          <ActionScreen
+            text="You are not allowed to access this page"
+            href="/"
+            buttonLabel="Return to home"
+          />
         )}
       </ConnectBlock>
     </Container>
   )
 }
 
-// Todo: This page
+// Todo: Test actionScreen logs
