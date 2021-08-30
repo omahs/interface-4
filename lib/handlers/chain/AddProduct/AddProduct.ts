01208f83bb15503245d14103ae5067240cf87e81
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-08-30 16:21:35 +0200

    fixed addProduct chain handler

--- lib/handlers/chain/AddProduct/AddProduct.ts
+++ lib/handlers/chain/AddProduct/AddProduct.ts
@@ -2,33 +2,19 @@ import { BigNumber } from "ethers"
 import { initialize } from "@lib/useProvider"
 import { slicer } from "@lib/initProvider"
 
-type Props = {
-  slicerId: number
-  categoryIndex: number
-  price: number
-  isUSD: boolean
-  isMultiple: boolean
-  isInfinite: boolean
-  units: number
-  data?: any
-  purchaseData?: any
-  subSlicersIds?: number[]
-  subProducts?: number[]
-}
-
-const AddProduct = async ({
-  slicerId,
-  categoryIndex,
-  price,
-  isUSD,
-  isMultiple,
-  isInfinite,
-  units,
+const AddProduct = async (
+  slicerId: number,
+  categoryIndex: number,
+  price: number,
+  isUSD: boolean,
+  isMultiple: boolean,
+  isInfinite: boolean,
+  units: number,
   data = [],
   purchaseData = [],
-  subSlicersIds,
-  subProducts,
-}: Props) => {
+  subSlicersIds = [],
+  subProducts = []
+) => {
   const { signer } = await initialize()
   const contract = await slicer(slicerId, signer)
   const decimals = BigNumber.from(10).pow(18)
