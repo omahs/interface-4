a0509be34ee5a835c0f157a324d18ce7c90b4fa1
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-11 20:33:47 +0200

    fixed corsMiddleware

--- utils/corsMiddleware.ts
+++ utils/corsMiddleware.ts
@@ -1,18 +1,18 @@
 import Cors from "cors"
 import initMiddleware from "@utils/initMiddleware"
 
+const whitelist = [process.env.NEXT_PUBLIC_APP_URL]
+
 const corsMiddleware = initMiddleware(
   Cors({
-    origin: "https://slice.so",
+    origin: function (origin, callback) {
+      console.log(origin)
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
   })
 )
 export default corsMiddleware
-
-// Alternative
-// origin: function (origin, callback) {
-//   if (whitelist.indexOf(origin) !== -1) {
-//     callback(null, true)
-//   } else {
-//     callback(new Error("Not allowed by CORS"))
-//   }
-// },
