ed75a022dd6c81d6bc98e6100df943e7a6aa675c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-19 20:25:33 +0200

    updated prisma schema

---
+++ prisma/migrations/20210919182518_delete_is_pending_from_revert/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isPending` on the `Revert` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Revert" DROP COLUMN "isPending";
