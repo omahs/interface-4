6fa2345fdf401585030a1170cd9ce92310121b03
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-19 20:34:02 +0200

    products api

--- pages/api/slicer/[id]/products.ts
+++ pages/api/slicer/[id]/products.ts
@@ -1,17 +1,68 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import { CreateProduct } from "@lib/handlers/prisma"
+import { CreateProduct, UpdateProduct } from "@lib/handlers/prisma"
 import prisma from "@lib/db"
 import corsMiddleware from "@utils/corsMiddleware"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await corsMiddleware(req, res)
-  const { id, productId } = req.query
+  const { id, productId, name, pending } = req.query
   let data: any
 
   try {
+    if (req.method === "GET") {
+      if (pending == "true") {
+        data = await prisma.product.findMany({
+          where: {
+            AND: [
+              {
+                slicerId: { equals: Number(id) },
+              },
+              {
+                productId: { equals: null },
+              },
+              {
+                createdAt: {
+                  lte: new Date(Date.now() - 1000 * 60 * 60),
+                },
+              },
+            ],
+          },
+        })
+      }
+      // else {
+      //   data = await prisma.product.count({
+      //     where: {
+      //       AND: [
+      //         {
+      //           slicerId: { equals: Number(id) },
+      //         },
+      //         {
+      //           name: { equals: String(name) },
+      //         },
+      //       ],
+      //     },
+      //   })
+      // }
+    }
+
     if (req.method === "POST") {
-      const { name, description, image, hash } = JSON.parse(req.body)
-      data = await CreateProduct(Number(id), name, description, hash, image)
+      const { name, description, image, creator, uid, hash, tempProductHash } =
+        JSON.parse(req.body)
+      data = await CreateProduct(
+        Number(id),
+        name,
+        description,
+        creator,
+        uid,
+        hash,
+        tempProductHash,
+        image
+      )
+    }
+
+    if (req.method === "PUT") {
+      const { id, tempProductHash, productId } = JSON.parse(req.body)
+      data = await UpdateProduct(Number(id), tempProductHash, Number(productId))
     }
 
     if (req.method === "DELETE") {
