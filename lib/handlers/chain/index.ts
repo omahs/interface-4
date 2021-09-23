4be6b1270a63ca1fd964ab66676ea0782cb2f329
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-23 13:42:18 +0200

    remove GetProductPrice handler

--- lib/handlers/chain/index.ts
+++ lib/handlers/chain/index.ts
@@ -1,5 +1,4 @@
 export { default as AddProduct } from "./AddProduct"
-export { default as GetProductPrice } from "./GetProductPrice"
 export { default as GetSlcBalance } from "./GetSlcBalance"
 export { default as PayProduct } from "./PayProduct"
 export { default as PayProducts } from "./PayProducts"
