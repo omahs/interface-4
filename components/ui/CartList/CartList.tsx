1259b9f56eb00180563c6ec7ab141d45c9520095
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-24 20:41:51 +0200

    made CartList

--- components/ui/CartList/CartList.tsx
+++ components/ui/CartList/CartList.tsx
@@ -1,34 +1,53 @@
-import Trash from "@components/icons/Trash"
+import { CookieSetOptions } from "universal-cookie"
 import { ProductCart } from "@lib/handleUpdateCart"
+import { ListCard } from ".."
 
 type Props = {
   cookieCart: ProductCart[]
   ethUsd: { symbol: string; price: number }
+  setCookie: (name: "cart", value: any, options?: CookieSetOptions) => void
 }
 
-const CartList = ({ cookieCart, ethUsd }: Props) => {
+const CartList = ({ cookieCart, ethUsd, setCookie }: Props) => {
   return (
-    <>
+    <div className="p-4 space-y-4 shadow-lg bg-sky-100 rounded-xl max-h-[300px] sm:max-h-[450px] max-w-[350px] overflow-y-scroll">
       {cookieCart?.map((product, key) => {
-        const { slicerId, slicerAddress, productId, quantity, price, isUSD } =
-          product
+        const {
+          slicerId,
+          slicerAddress,
+          productId,
+          quantity,
+          price,
+          isUSD,
+          name,
+        } = product
         const productPrice = isUSD
           ? Math.floor((price * 10) / Number(ethUsd?.price)) / 1000
           : Math.floor(price / 10 ** 14) / 10000
+
+        const index = cookieCart.findIndex(
+          (p: ProductCart) => p.productId == productId
+        )
+
+        const handleRemove = () => {
+          const newCookies = cookieCart
+          newCookies.splice(index, 1)
+          setCookie("cart", newCookies)
+        }
         return (
-          <div key={key}>
-            <p>
-              {slicerId} / #{productId}
-            </p>
-            <p>
-              {productPrice} (x{quantity})
-            </p>
-            {/* Plus / minus */}
-            <Trash /> {/* remove */}
-          </div>
+          <ListCard
+            key={key}
+            label={name}
+            sideLabel={`${slicerId} / #${productId}`}
+            subLabel={`Ξ ${productPrice}${
+              quantity != 1 ? ` x ${quantity}` : ""
+            }`}
+            href={`/slicer/${slicerId}`}
+            remove={() => handleRemove()}
+          />
         )
       })}
-    </>
+    </div>
   )
 }
 
