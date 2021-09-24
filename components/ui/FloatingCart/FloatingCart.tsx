7f1d78de0278d2f82edfa9efc3c3263a179c5341
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-24 18:43:02 +0200

    completed FloatingCart

--- components/ui/FloatingCart/FloatingCart.tsx
+++ components/ui/FloatingCart/FloatingCart.tsx
@@ -1,13 +1,23 @@
+import Arrow from "@components/icons/Arrow"
+import Chevron from "@components/icons/Chevron"
+import ShoppingBag from "@components/icons/ShoppingBag"
+import handleConnect from "@lib/handleConnect"
+import { PayProducts } from "@lib/handlers/chain"
 import { ProductCart } from "@lib/handleUpdateCart"
 import fetcher from "@utils/fetcher"
+import { useEffect, useState } from "react"
 import { useCookies } from "react-cookie"
 import useSWR from "swr"
 import { CartList } from ".."
+import { useAppContext } from "../context"
 
 type Props = {}
 
 const FloatingCart = ({}: Props) => {
+  const { isConnected } = useAppContext()
   const [cookies] = useCookies(["cart"])
+  const [showCart, setShowCart] = useState(false)
+  const [showCartList, setShowCartList] = useState(false)
   const cookieCart: ProductCart[] = cookies?.cart
 
   const { data: ethUsd } = useSWR(
@@ -24,17 +34,64 @@ const FloatingCart = ({}: Props) => {
   }
   const totalPrice: number = cookieCart?.reduce(reducer, 0) || 0
 
+  console.log(cookieCart)
+
+  useEffect(() => {
+    if (cookieCart && cookieCart?.length != 0) {
+      setShowCart(true)
+    } else {
+      setShowCart(false)
+    }
+  }, [cookieCart])
+
+  const handleCheckout = async () => {
+    await PayProducts(cookieCart)
+  }
+
   return (
-    <div
-      className={`fixed bottom-0 mb-[20px] right-[20px] transition-opacity duration-200 ${
-        cookieCart?.length != 0 ? "z-20 opacity-100" : "-z-10 opacity-0"
-      }`}
-    >
-      <div className="px-16 py-4 bg-blue-600 rounded-full">
-        {/* <CartList cookieCart={cookieCart} ethUsd={ethUsd} /> */}
-        {totalPrice != 0 && <p>{Math.round(totalPrice * 1000) / 1000}</p>}
+    <>
+      <div
+        className={`fixed bottom-0 mb-[80px] right-[20px] nightwind-prevent-block transition-opacity duration-200 ${
+          showCartList ? "z-20 opacity-100" : "-z-10 opacity-0"
+        }`}
+      >
+        <CartList cookieCart={cookieCart} ethUsd={ethUsd} />
       </div>
-    </div>
+      <div
+        className={`fixed bottom-0 mb-[20px] right-[20px] nightwind-prevent-block transition-opacity duration-200 ${
+          showCart ? "z-20 opacity-100" : "-z-10 opacity-0"
+        }`}
+      >
+        <div className="flex h-12 pl-3 overflow-hidden font-medium text-black bg-white border-2 border-transparent rounded-full shadow-base">
+          <div
+            className="flex items-center pl-2 pr-4 min-w-[100px] cursor-pointer group"
+            onClick={() => setShowCartList((showCartList) => !showCartList)}
+          >
+            {totalPrice != 0 && (
+              <>
+                <Chevron
+                  className={`h-5 transition-transform duration-200 ${
+                    showCartList ? "rotate-90" : ""
+                  } w-7`}
+                />
+                <p className="w-full ml-2 text-center">
+                  Ξ {Math.round(totalPrice * 1000) / 1000}
+                </p>
+              </>
+            )}
+          </div>
+          <div
+            className="flex items-center h-full px-4 text-sm text-white transition-colors duration-150 bg-blue-600 cursor-pointer hover:bg-green-500 nightwind-prevent"
+            onClick={() => (isConnected ? handleCheckout() : handleConnect())}
+          >
+            <p className="pr-2 text-sm ">
+              {isConnected ? "Checkout" : "Connect"}
+            </p>
+            <ShoppingBag className="w-[18px] h-[18px]" />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
