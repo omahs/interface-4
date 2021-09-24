e93e023fcea770be23c2081af23b34e8664039e8
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-24 20:41:45 +0200

    added ListCard component

---
+++ components/ui/ListCard/ListCard.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+import Delete from "@components/icons/Delete"
+
+type Props = {
+  label: string
+  sideLabel: string
+  subLabel: string
+  href?: string
+  remove?: () => void
+}
+
+const ListCard = ({ label, sideLabel, subLabel, href, remove }: Props) => {
+  const content = (
+    <div className="grid items-center h-16 grid-cols-6 pl-4 pr-2 bg-white border border-gray-100 shadow-base rounded-xl ">
+      <div className="flex justify-center mr-2 text-xs">{sideLabel}</div>
+      <div className="col-span-4 px-1 text-left">
+        <p className="font-medium truncate">{label}</p>
+        <p className="text-sm text-gray-400">{subLabel}</p>
+      </div>
+      <div className="flex justify-center">
+        <Delete onClick={() => remove()} />
+      </div>
+    </div>
+  )
+  return (
+    <div>
+      {href ? (
+        <Link href={href}>
+          <a>{content}</a>
+        </Link>
+      ) : (
+        content
+      )}
+    </div>
+  )
+}
+
+export default ListCard
