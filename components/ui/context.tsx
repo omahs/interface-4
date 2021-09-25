11aee5cbe49b0dcf09145182978ab91b4ae25520
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-25 14:20:40 +0200

    added purchases to context

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -2,6 +2,14 @@ import useProvider from "@lib/useProvider"
 import { createContext, useContext, useEffect, useState } from "react"
 import { colorList, darkColorList } from "@utils/colorList"
 import { View } from "@lib/text/modals"
+import client from "@utils/apollo-client"
+import { gql } from "@apollo/client"
+
+export type Purchase = {
+  slicerId: string
+  productId: string
+  quantity: string
+}
 
 const AppContext = createContext<any>({
   isConnected: false,
@@ -15,6 +23,7 @@ const AppContext = createContext<any>({
   modalView: { name: "" },
   setModalView: () => null,
   shuffleColors: () => null,
+  purchases: [],
 })
 
 export function AppWrapper({ children }) {
@@ -26,6 +35,7 @@ export function AppWrapper({ children }) {
   const [color2, setColor2] = useState([])
   const [darkColor1, setDarkColor1] = useState([])
   const [darkColor2, setDarkColor2] = useState([])
+  const [purchases, setPurchases] = useState<Purchase[]>([])
 
   const shuffleColors = () => {
     const random1 = Math.floor(Math.random() * colorList.length)
@@ -42,9 +52,50 @@ export function AppWrapper({ children }) {
     root.style.setProperty("--darkColor2", darkColorList[random2][0])
   }
 
+  const getPurchases = async (buyer: string) => {
+    const tokensQuery = /* GraphQL */ `
+        payee (id: "${buyer.toLowerCase()}") {
+        purchases {
+          id
+          quantity
+        }
+      }`
+
+    const { data } = await client.query({
+      query: gql`
+        query {
+          ${tokensQuery}
+        }
+      `,
+    })
+    const payeePurchases = data?.payee?.purchases
+    let purchasesList: Purchase[] = []
+    payeePurchases.map((p) => {
+      const id = p.id.split("-")
+      const slicerId = id[0]
+      const productId = id[1]
+      purchasesList.push({ slicerId, productId, quantity: p.quantity })
+    })
+    setPurchases(purchasesList)
+  }
+
   useEffect(() => {
     shuffleColors()
   }, [])
+
+  useEffect(() => {
+    if (account) {
+      getPurchases(account)
+    }
+  }, [account])
+
+  // Todo:
+
+  // useEffect(() => {
+  //   if (purchases && cookies) {
+  //     // Remove from cart products which have already been purchased & are not multiple
+  //   }
+  // }, [purchases, cookies])
 
   return (
     <AppContext.Provider
@@ -60,6 +111,7 @@ export function AppWrapper({ children }) {
         modalView,
         setModalView,
         shuffleColors,
+        purchases,
       }}
     >
       {children}
