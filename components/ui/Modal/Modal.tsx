6eec72aa2ed8ce15e23b1d1e3fa3c0e7d1787f8f
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-09-26 02:08:33 +0200

    modal styling

--- components/ui/Modal/Modal.tsx
+++ components/ui/Modal/Modal.tsx
@@ -37,21 +37,23 @@ const Modal = ({ modalView, setModalView }: Props) => {
   }
 
   return (
-    <div className="fixed top-0 z-10 flex items-center justify-center w-screen h-screen background-modal">
+    <div className="fixed top-0 z-10 w-screen h-screen py-12 overflow-y-scroll xs:py-20 background-modal">
       <div
-        className="absolute w-full h-full"
+        className="absolute w-full h-full mt-[-3rem] xs:mt-[-5rem]"
         onClick={() => (cross ? setModalView({ name: "" }) : null)}
       />
-      <div className="relative w-full px-4 py-8 mx-4 bg-white shadow-xl xs:py-12 xs:px-8 rounded-xl max-w-screen-xs">
-        {cross && (
-          <div className="absolute top-[24px] right-[24px]">
-            <Cross
-              className="text-right cursor-pointer hover:text-red-500"
-              onClick={() => setModalView({ name: "" })}
-            />
-          </div>
-        )}
-        <div>{content}</div>
+      <div className="flex items-center justify-center">
+        <div className="relative w-full px-4 py-8 mx-4 bg-white shadow-xl xs:py-12 xs:px-8 rounded-xl max-w-screen-xs">
+          {cross && (
+            <div className="absolute top-[24px] right-[24px]">
+              <Cross
+                className="text-right cursor-pointer hover:text-red-500"
+                onClick={() => setModalView({ name: "" })}
+              />
+            </div>
+          )}
+          <div>{content}</div>
+        </div>
       </div>
     </div>
   )
