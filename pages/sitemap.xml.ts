78d91c7f66f99861c7bdc6a6649db409d8274913
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-10-05 00:34:36 +0200

    added dynamic sitemap

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,48 @@
+import fetcher from "@utils/fetcher"
+import { GetServerSideProps } from "next"
+
+function generateSiteMap(totalSlicers) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+        <loc>https://slice.so/</loc>
+      </url>
+      <url>
+        <loc>https://slice.so/slicer</loc>
+      </url>
+     ${[...Array(totalSlicers)]
+       .map((el, key) => {
+         const slicerId = Number(key)
+         return `
+       <url>
+           <loc>${`https://slice.so/slicer/${slicerId}`}</loc>
+       </url>
+     `
+       })
+       .join("")}
+   </urlset>
+ `
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL
+  const { totalSlicers } = await fetcher(`${baseUrl}/api/slicer/total`)
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(totalSlicers)
+
+  res.setHeader("Content-Type", "text/xml")
+  // we send the XML to the browser
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+export default SiteMap
