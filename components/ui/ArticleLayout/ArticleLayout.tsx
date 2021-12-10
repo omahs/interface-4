b5aea08d40cbb4ce275e84333ca8286ec731326c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-12-10 15:48:55 +0100

    update post content and components

--- components/ui/ArticleLayout/ArticleLayout.tsx
+++ components/ui/ArticleLayout/ArticleLayout.tsx
@@ -6,7 +6,17 @@ import { Back, Container } from "@components/ui"
 import { NextSeo } from "next-seo"
 import { domain } from "@components/common/Head"
 
-export default function Article(props) {
+export default function Article({
+  htmlTitle,
+  title,
+  subtitle,
+  slug,
+  author,
+  authorLink,
+  ogImage,
+  coverImage,
+  children,
+}) {
   useEffect(() => {
     const script = document.createElement("script")
     script.setAttribute("src", "https://platform.twitter.com/widgets.js")
@@ -19,19 +29,19 @@ export default function Article(props) {
     <>
       <Container page={true}>
         <NextSeo
-          title={props.htmlTitle || props.title}
-          description={props.subtitle}
+          title={htmlTitle || title}
+          description={subtitle}
           titleTemplate="%s | Slice Blog"
           openGraph={{
-            title: `${props.htmlTitle || props.title} | Slice blog`,
-            description: props.subtitle,
-            url: `${domain}/blog/${props.slug}`,
+            title: `${htmlTitle || title} | Slice blog`,
+            description: subtitle,
+            url: `${domain}/blog/${slug}`,
             images: [
               {
-                url: props.coverImage
-                  ? `${domain}/blog/${props.coverImage}`
+                url: coverImage
+                  ? `${domain}/blog/${ogImage}`
                   : `${domain}/og_image.jpg`,
-                alt: `${props.slug} blog post cover image`,
+                alt: `${slug} blog post cover image`,
               },
             ],
           }}
@@ -40,8 +50,8 @@ export default function Article(props) {
           <meta
             name="twitter:image"
             content={
-              props.coverImage
-                ? `${domain}/blog/${props.coverImage}`
+              coverImage
+                ? `${domain}/blog/${coverImage}`
                 : `${domain}/twitter_card.jpg`
             }
           />
@@ -50,21 +60,38 @@ export default function Article(props) {
         <section className="max-w-screen-sm pb-10 mx-auto text-left">
           <Back />
           <div className="py-4">
-            <h1 className="pb-1 text-3xl">{props.title}</h1>
-            <h2 className="text-lg font-normal">{props.subtitle}</h2>
+            <h1 className="pb-2 text-3xl sm:text-4xl">{title}</h1>
+            <h2 className="text-lg font-normal">{subtitle}</h2>
+            {author && (
+              <p className="pt-3 text-sm text-gray-500">
+                by{" "}
+                {authorLink ? (
+                  <a
+                    href={authorLink}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-semibold text-gray-600"
+                  >
+                    {author}
+                  </a>
+                ) : (
+                  author
+                )}
+              </p>
+            )}
           </div>
-          {props.coverImage ? (
+          {coverImage ? (
             <div className="relative mt-5 mb-8 rounded-xl overflow-hidden height-[40vw] max-height-[270px]">
               <Image
-                src={props.coverImage}
-                alt={`Cover image of ${props.slug} post`}
+                src={coverImage}
+                alt={`Cover image of ${slug} post`}
                 layout="fill"
                 className="object-cover"
               />
             </div>
           ) : null}
 
-          <article className="prose">{props.children}</article>
+          <article className="prose">{children}</article>
         </section>
       </Container>
     </>
--- components/ui/Back/Back.tsx
+++ components/ui/Back/Back.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link"
 import Chevron from "@components/icons/Chevron"
 
-export default function Back() {
+export default function Back({ label = "Go back" }) {
   return (
     <div className="inline-block">
       <Link href="/blog">
-        <a className="text-sm group">
+        <a className="text-sm text-gray-500 group">
           <div className="flex items-center">
             <div className="w-6 h-6 mr-1 transition-transform duration-200 transform group-hover:-translate-x-1">
               <Chevron />
             </div>
-            <p className="mt-0.5 font-normal">Go back</p>
+            <p className="mt-0.5 font-normal">{label}</p>
           </div>
         </a>
       </Link>
