dc1a74228d3f548563c56d0930657e00127f8c13
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-12-12 03:05:04 +0100

    added HomeBanner component

---
+++ components/ui/HomeBanner/HomeBanner.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link"
+import { Banner, Button } from "@components/ui"
+import { accounts } from "../Social/Social"
+
+const HomeBanner = () => {
+  return (
+    <Banner title="Contribute to the DAO" id="contribute" inverted>
+      <div className="mx-auto space-y-6 max-w-screen-xs">
+        <p>
+          If you&apos;re excited about what we&apos;re building, consider
+          supporting Slice by funding the DAO treasury on Juicebox.
+        </p>
+        <p>
+          In exchange for your contribution you&apos;ll receive{" "}
+          <b>SLX governance tokens</b>. The sooner you do it, the more tokens
+          you&apos;ll receive.
+        </p>
+        <div className="pt-1 pb-4">
+          <Button
+            label="Go to treasury"
+            href="https://juicebox.money/#/p/slice"
+            external
+          />
+        </div>
+        <p className="text-sm text-gray-300">
+          Check out{" "}
+          <Link href="/blog/treasury">
+            <a className="text-white highlight highlight-inverted">this post</a>
+          </Link>{" "}
+          for more info, and if you have questions just reach out on{" "}
+          <a
+            className="text-white highlight highlight-inverted"
+            href={accounts.discord}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Discord
+          </a>
+        </p>
+      </div>
+    </Banner>
+  )
+}
+
+export default HomeBanner
