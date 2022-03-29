69947d82854ffd200f591b75102550305c4dfdd1
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-03-29 04:03:38 +0200

    added new contracts to initProvider

--- lib/initProvider.ts
+++ lib/initProvider.ts
@@ -4,13 +4,17 @@ import ProductsModuleContract from "artifacts/contracts/ProductsModule.sol/Produ
 import FundsModuleContract from "artifacts/contracts/FundsModule.sol/FundsModule.json"
 import SlicerContract from "artifacts/contracts/Slicer.sol/Slicer.json"
 import ChainlinkContract from "artifacts/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol/AggregatorV3Interface.json"
+import { SliceCore } from "../contracts/typechain-types/SliceCore"
+import { ProductsModule } from "../contracts/typechain-types/ProductsModule"
+import { FundsModule } from "../contracts/typechain-types/FundsModule"
+import { Slicer } from "../contracts/typechain-types/Slicer"
 
 export const sliceCore = (signer: ethers.Signer | ethers.providers.Provider) =>
   new ethers.Contract(
     process.env.NEXT_PUBLIC_SLICECORE_ADDRESS,
     SliceCoreContract.abi,
     signer
-  )
+  ) as SliceCore
 export const productsModule = (
   signer: ethers.Signer | ethers.providers.Provider
 ) =>
@@ -18,7 +22,7 @@ export const productsModule = (
     process.env.NEXT_PUBLIC_PRODUCTS_ADDRESS,
     ProductsModuleContract.abi,
     signer
-  )
+  ) as ProductsModule
 export const fundsModule = (
   signer: ethers.Signer | ethers.providers.Provider
 ) =>
@@ -26,14 +30,18 @@ export const fundsModule = (
     process.env.NEXT_PUBLIC_FUNDS_ADDRESS,
     FundsModuleContract.abi,
     signer
-  )
+  ) as FundsModule
 
 export const slicer = async (
   slicerId: number,
   signer: ethers.Signer | ethers.providers.Provider
 ) => {
   const slicerAddress = await sliceCore(signer).slicers(slicerId)
-  return new ethers.Contract(slicerAddress, SlicerContract.abi, signer)
+  return new ethers.Contract(
+    slicerAddress,
+    SlicerContract.abi,
+    signer
+  ) as Slicer
 }
 
 export const chainlink = (signer: ethers.Signer | ethers.providers.Provider) =>
