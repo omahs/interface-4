5a8b450abf576efab17fcaa80095d393537fff41
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-07-02 00:20:18 +0200

    added refresh products in SlicerProducts

--- components/ui/SlicerProducts/SlicerProducts.tsx
+++ components/ui/SlicerProducts/SlicerProducts.tsx
@@ -47,6 +47,7 @@ const SlicerProducts = ({
   const [loading, setLoading] = useState(false)
   const [showProducts, setShowProducts] = useState<Product[]>([])
   const [pendingProducts, setPendingProducts] = useState<Product[]>([])
+  const [subgraphRefresh, setSubgraphRefresh] = useState(false)
   const router = useRouter()
 
   const handleReload = async () => {
@@ -64,8 +65,25 @@ const SlicerProducts = ({
     router.reload()
   }
 
+  const refreshProducts = async () => {
+    const fetcher = (await import("@utils/fetcher")).default
+    await fetcher(`/api/slicer/${slicerId}/refresh`)
+    router.reload()
+  }
+
   useEffect(() => {
-    setShowProducts(products?.data?.filter((p: Product) => p.productId != null))
+    const productsToShow = products?.data?.filter(
+      (p: Product) => p.productId != null
+    )
+    setShowProducts(productsToShow)
+    setSubgraphRefresh(
+      productsToShow.filter(
+        (product: Product) =>
+          !blockchainProducts?.find(
+            (p) => p.id.split("-").pop() == product.productId
+          )
+      ).length != 0
+    )
     setPendingProducts(products?.data?.filter((p: Product) => !p.productId))
   }, [products])
 
@@ -88,20 +106,35 @@ const SlicerProducts = ({
               />
             </div>
           )}
-          {(pendingProducts?.length != 0 ||
-            blockchainProducts?.length > products?.data?.length) && (
-            <div className="pt-12">
-              <p className="pb-8">
-                There are pending products for your slicer. <br />
-                Click the button below to reload them.
+          {subgraphRefresh ? (
+            <>
+              <hr className="w-20 mx-auto mt-16 mb-12 border-gray-300" />
+              <p className="pb-8 font-semibold text-yellow-600">
+                Some product info has to be retrieved from the blockchain.
               </p>
               <Button
-                label="Reload products"
+                label="Refresh products"
                 type="button"
                 loading={loading}
-                onClick={async () => await handleReload()}
+                onClick={async () => await refreshProducts()}
               />
-            </div>
+            </>
+          ) : (
+            (pendingProducts?.length != 0 ||
+              blockchainProducts?.length > products?.data?.length) && (
+              <>
+                <hr className="w-20 mx-auto mt-16 mb-12 border-gray-300" />
+                <p className="pb-8 font-semibold text-yellow-600">
+                  There are pending products for your slicer.
+                </p>
+                <Button
+                  label="Reload products"
+                  type="button"
+                  loading={loading}
+                  onClick={async () => await handleReload()}
+                />
+              </>
+            )
           )}
           <hr className="w-20 mx-auto mt-16 border-gray-300" />
         </div>
