c29cd8c13c2f213cd5a0e49cbeeec4268e780c9d
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-07-02 20:58:14 +0200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,61 +54,54 @@ const Home = () => {
     })
   }, [])
 
-  const Home = () => {
-    return (
-      <>
-        <NextSeo
-          openGraph={{
-            title: longTitle,
-            description: defaultDescription,
-            url: domain,
-            images: [
-              {
-                url: `${domain}/og_image.jpg`,
-                width: 1000,
-                height: 1000,
-                alt: `${defaultTitle} cover image`
+  return (
+    <>
+      <NextSeo
+        openGraph={{
+          title: longTitle,
+          description: defaultDescription,
+          url: domain,
+          images: [
+            {
+              url: `${domain}/og_image.jpg`,
+              width: 1000,
+              height: 1000,
+              alt: `${defaultTitle} cover image`
+            }
+          ]
+        }}
+      />
+      <Container page={true}>
+        <main className="max-w-screen-lg pb-10 mx-auto text-center">
+          <HomeHero />
+          <div className="space-y-28 sm:space-y-52">
+            <HomeSection text={section1} image={<HomeCake />} />
+            <HomeSection
+              text={section2}
+              image={
+                <Logo interactive size="w-32 sm:w-36" margin="mt-4 ml-6" spin />
               }
-            ]
-          }}
-        />
-        <Container page={true}>
-          <main className="max-w-screen-lg pb-10 mx-auto text-center">
-            <HomeHero />
-            <div className="space-y-28 sm:space-y-52">
-              <HomeSection text={section1} image={<HomeCake />} />
-              <HomeSection
-                text={section2}
-                image={
-                  <Logo
-                    interactive
-                    size="w-32 sm:w-36"
-                    margin="mt-4 ml-6"
-                    spin
-                  />
-                }
-                side="right"
-              />
-              <HomeSection
-                text={section3}
-                image={<HomeEth size="w-32 sm:w-36" />}
-              />
-              {/* <HomeSection
+              side="right"
+            />
+            <HomeSection
+              text={section3}
+              image={<HomeEth size="w-32 sm:w-36" />}
+            />
+            {/* <HomeSection
               text={section4}
               image={<HomeEth size="w-32 sm:w-36" />}
               side="right"
             /> */}
-            </div>
-          </main>
-          <Examples />
-        </Container>
-        <HomeBanner />
-        <Container page={true}>
-          <FAQs />
-        </Container>
-      </>
-    )
-  }
+          </div>
+        </main>
+        <Examples />
+      </Container>
+      <HomeBanner />
+      <Container page={true}>
+        <FAQs />
+      </Container>
+    </>
+  )
 }
 
 export default Home
