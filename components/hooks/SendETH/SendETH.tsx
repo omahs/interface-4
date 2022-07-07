a931191066620d94bc544ce71f9ede6fab44be74
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-07-07 16:48:35 +0200

    SendETH hook

---
+++ components/hooks/SendETH/SendETH.tsx
@@ -0,0 +1,74 @@
+import { InputAddress, InputPrice } from "@components/ui"
+import { ethers, BigNumber } from "ethers"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { Hook } from "../purchaseHooks"
+
+const label = "Send ETH to address"
+
+const description = "Send ETH to an external address"
+
+type Props = {
+  setParams: Dispatch<SetStateAction<any>>
+}
+
+const Component = ({ setParams }: Props) => {
+  const [address, setAddress] = useState("")
+  const [resolvedAddress, setResolvedAddress] = useState("")
+  const [usdValue, setUsdValue] = useState(0)
+  const [ethValue, setEthValue] = useState(0)
+
+  useEffect(() => {
+    const externalAddress = address
+      ? address.substring(address.length - 4) !== ".eth"
+        ? resolvedAddress != "Invalid ENS name"
+          ? address
+          : ethers.constants.AddressZero
+        : resolvedAddress
+      : ethers.constants.AddressZero
+    const decimals = BigNumber.from(10).pow(9)
+    const ethToWei = BigNumber.from((ethValue * 10 ** 9).toFixed(0)).mul(
+      decimals
+    )
+    setParams({
+      externalCall: {
+        data: [],
+        value: ethToWei,
+        externalAddress,
+        checkFunctionSignature: "0x00000000",
+        execFunctionSignature: "0x00000000"
+      }
+    })
+  }, [ethValue, usdValue, address, resolvedAddress])
+
+  return (
+    <>
+      <div>
+        <InputAddress
+          label="Address"
+          address={address}
+          onChange={setAddress}
+          required
+          resolvedAddress={resolvedAddress}
+          setResolvedAddress={setResolvedAddress}
+        />
+      </div>
+      <div className="py-3">
+        <InputPrice
+          ethValue={ethValue}
+          setEthValue={setEthValue}
+          usdValue={usdValue}
+          setUsdValue={setUsdValue}
+          label="Value per unit"
+        />
+      </div>
+
+      <p className="text-yellow-600">
+        <b>The sent value will be added to the unit product price.</b>
+      </p>
+    </>
+  )
+}
+
+const hook: Hook = { label, description, Component }
+
+export default hook
