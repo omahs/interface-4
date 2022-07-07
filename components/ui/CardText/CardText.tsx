96b6c32cf7cc9fba076881462338b4de497cad86
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-07-07 16:48:44 +0200

    CardText styling

--- components/ui/CardText/CardText.tsx
+++ components/ui/CardText/CardText.tsx
@@ -5,7 +5,11 @@ type Props = {
 
 export default function CardText({ label, isActive }: Props) {
   return (
-    <div className="flex items-center justify-between px-5 py-4 font-medium text-left transition-colors duration-150 bg-white border-2 border-blue-300 rounded-lg cursor-pointer hover:border-blue-600 shadow-light-focusable">
+    <div
+      className={`flex items-center justify-between px-5 py-4 font-medium text-left transition-colors duration-150 bg-white border-2 rounded-lg cursor-pointer shadow-light-focusable ${
+        isActive ? "border-blue-600" : "border-blue-300 hover:border-blue-600"
+      }`}
+    >
       <p>{label}</p>
       {isActive && <p>🍰</p>}
     </div>
