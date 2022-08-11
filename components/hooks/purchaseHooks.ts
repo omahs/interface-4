a461863aa43f1e263617304c8ecf26bb33764ef0
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-08-11 13:54:05 +0200

    add erc721gated to default hooks

--- components/hooks/purchaseHooks.ts
+++ components/hooks/purchaseHooks.ts
@@ -43,6 +43,7 @@ export const emptyExternalCall = {
 
 export const defaultPurchaseHooks: Hook[] = [
   hooks.ERC20Gated,
+  hooks.ERC721Gated,
   hooks.Allowlisted,
   hooks.SendETH,
   hooks.ExistingHook
