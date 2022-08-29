c9c478dcd8ff711ea014e670ccc8c42ea08ad84b
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-08-29 10:07:29 +0200

    trigger-deploy

--- pages/slicer/[id]/index.tsx
+++ pages/slicer/[id]/index.tsx
@@ -456,7 +456,7 @@ export default Id
 // TODO
 // - retrieve account.slices in editAllowed condition
 // - Clean stuff
-
+//
 /**  
 Product created on interface - OK
   - Present on backend, not subgraph
