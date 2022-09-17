d4b368ed4fe99adc6b58aa21a0520a06134289c0
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-09-17 21:54:11 +0200

    fixed toWad

--- utils/toWad.ts
+++ utils/toWad.ts
@@ -2,8 +2,8 @@ import { ethers } from "ethers"
 
 const toWad = (value: number) => {
   return ethers.BigNumber.from(10)
-    .pow(15)
-    .mul(Math.floor(Number(value) * 1000))
+    .pow(13)
+    .mul(Math.floor(value * 100000))
 }
 
 export default toWad
