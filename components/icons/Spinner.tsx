e664a344c74c1e888fed1cb3abd917ca01eb4675
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-09-29 01:36:25 +0200

    fixed spinner

--- components/icons/Spinner.tsx
+++ components/icons/Spinner.tsx
@@ -1,7 +1,7 @@
 const Spinner = ({ ...props }) => {
   return (
     <svg
-      className={`animate-spin ${props.size ? props.size : " -ml-1 h-5 w-5"}`}
+      className={`animate-spin ${props.size ? props.size : "h-5 w-5"}`}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
